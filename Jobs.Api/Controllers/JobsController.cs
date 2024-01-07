69759ef000f2026d14cc4ebabf8b2922a26edd5a
danieldantasdev <contatodanieldantasdev@gmail.com>
2024-01-07 17:48:08 -0300

    feat: adição dos controladores exigidos no teste

---
+++ Jobs.Api/Controllers/JobsController.cs
@@ -0,0 +1,47 @@
+﻿using Jobs.Api.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jobs.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        // GET api/jobs
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        // GET api/jobs/123
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            //return NotFound();
+            return Ok();
+        }
+
+        // POST api/jobs
+        [HttpPost]
+        public IActionResult Post(Job job)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = job.Id }, job);
+        }
+
+        // PUT api/jobs/123
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Job job)
+        {
+            return NoContent();
+        }
+
+        // DELETE api/jobs/123
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
