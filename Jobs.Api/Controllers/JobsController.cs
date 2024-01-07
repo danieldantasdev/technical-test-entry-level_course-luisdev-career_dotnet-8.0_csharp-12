aa1943d3fcd637acbcfb521ded3b04641f9f140e
danieldantasdev <contatodanieldantasdev@gmail.com>
2024-01-07 18:38:51 -0300

    feat: adição do banco de dados em memória

--- Jobs.Api/Controllers/JobsController.cs
+++ Jobs.Api/Controllers/JobsController.cs
@@ -1,39 +1,67 @@
 ﻿using Jobs.Api.Entities;
-using Microsoft.AspNetCore.Http;
+using Jobs.Api.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Jobs.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class JobsController : ControllerBase
+    public class JobsController(JobsDbContext jobsDbContext) : ControllerBase
     {
+        private readonly JobsDbContext _jobsDbContext = jobsDbContext;
+
         // GET api/jobs
         [HttpGet]
         public IActionResult GetAll()
         {
-            return Ok();
+            List<Job> jobs = [.. _jobsDbContext.Jobs.AsNoTracking()];
+            //var jobs = _jobsDbContext.Jobs.AsNoTracking().ToList();
+            return Ok(jobs);
         }
 
         // GET api/jobs/123
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            //return NotFound();
-            return Ok();
+            Job? job = _jobsDbContext
+                .Jobs
+                .AsNoTracking()
+                .SingleOrDefault(j => j.Id == id);
+
+            if (job is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(job);
         }
 
         // POST api/jobs
         [HttpPost]
-        public IActionResult Post(Job job)
+        public IActionResult Post(Job jobInputModel)
         {
-            return CreatedAtAction(nameof(GetById), new { id = job.Id }, job);
+            _jobsDbContext.Jobs.Add(jobInputModel);
+            _jobsDbContext.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById), new { id = jobInputModel.Id }, jobInputModel);
         }
 
         // PUT api/jobs/123
         [HttpPut("{id}")]
-        public IActionResult Put(int id, Job job)
+        public IActionResult Put(int id, Job jobInputModel)
         {
+            Job? job = _jobsDbContext.Jobs.SingleOrDefault(j => j.Id == id);
+
+            if (job is null)
+            {
+                return NotFound();
+            }
+
+            job.Update(jobInputModel);
+            _jobsDbContext.Update(job);
+            _jobsDbContext.SaveChanges();
+
             return NoContent();
         }
 
@@ -41,6 +69,16 @@ namespace Jobs.Api.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
+            Job? job = _jobsDbContext.Jobs.SingleOrDefault(j => j.Id == id);
+
+            if (job is null)
+            {
+                return NotFound();
+            }
+
+            _jobsDbContext.Jobs.Remove(job);
+            _jobsDbContext.SaveChanges();
+
             return NoContent();
         }
     }
